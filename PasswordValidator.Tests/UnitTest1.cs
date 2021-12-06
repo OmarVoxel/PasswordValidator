c8fa2830bde06b847f2da7933443a5e65893fb32
oizquierdo <Oizquierdo@voxelgroup.net>
2021-12-06 20:49:26 +0100

    test: BeAtLeast8OrMoreCharacters

--- PasswordValidator.Tests/UnitTest1.cs
+++ PasswordValidator.Tests/UnitTest1.cs
@@ -1,13 +1,20 @@
 using System;
 using Xunit;
+using FluentAssertions;
 
 namespace PasswordValidator.Tests
 {
-    public class UnitTest1
+    public class PasswordShould
     {
         [Fact]
-        public void Test1()
+        public void BeAtLeast8OrMoreCharacters()
         {
+            PasswordValidator password = new("12345");
+            
+            ValidationResult result = password.Validation();
+            
+            result.IsValid.Should().Be(false);
+            result.Message.Should().Be("Password must be at least 8 characters");
         }
     }
 }
---
+++ PasswordValidator/PasswordValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PasswordValidator
+{
+    public class PasswordValidator
+    {
+        private readonly string _password;
+
+        public PasswordValidator(string password)
+        {   
+            _password = password;
+        }
+        public ValidationResult Validation()
+        {
+            return new ValidationResult(true, null);
+        }
+    }
+
+    public class ValidationResult
+    {
+        public string Message { get; }
+        public bool IsValid { get; }
+
+        public ValidationResult(bool isValid, string message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+    }
+}
